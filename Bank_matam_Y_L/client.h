ac7d856f65b83044c8704d754719f5c1ac86e666
yair pinhas <48602767+yp6969@users.noreply.github.com>
2020-01-17 16:31:52 +0200

    Add files via upload

--- Bank_matam_Y_L/client.h
+++ Bank_matam_Y_L/client.h
@@ -1,7 +1,7 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
-#include "updates.h"
+#include "generics.h"
 
 #define FOREVER while(1)
 #define IS_LEAF(t) (!(t)->left && !(t)->right)
@@ -13,31 +13,31 @@
 
 /*  functions that handle the client tree data*/
 void addClientToBranch_t(Branch* branch);
-void createBranchClientTree( Client_tree* clientHead );
-Client_tree * addNewClientToBranch(Client_tree * clientHead , Client client ) ;
+void createBranchClientTree( Tree* clientHead );
+Tree * addNewClientToBranch(Tree * clientHead , Client client ) ;
 Client* createNewClient(int btanchId);
 
 
 /* searching clients */
 void findClient(D_Llinked_List* list );
-void findClientInBankByAcountBalance(D_Llinked_List* list ,Branch_tree* branchHead ,  int acountBalance );
-void findClientInBranchByAcountBalance(D_Llinked_List* list , Client_tree* clientHead , int acountBalance);
-Client* searchClientByIdInBank(Branch_tree* branchHead ,  int clientId );
-Client* searchClientById( Client_tree* clientHead , int clientId );
+void findClientInBankByAcountBalance(D_Llinked_List* list ,Tree* branchHead ,  int acountBalance );
+void findClientInBranchByAcountBalance(D_Llinked_List* list , Tree* clientHead , int acountBalance);
+Client* searchClientByIdInBank(Tree* branchHead ,  int clientId );
+Client* searchClientById( Tree* clientHead , int clientId );
 void addNode(D_Llinked_List* list , Client* newClient );
 
 
 /*      searching branch       */
-Branch* searchBranchById(Branch_tree* branchHead , int branchId );
+Branch* searchBranchById(Tree* branchHead , int branchId );
 
 
 /*  deleting clients */
-Client_tree* deleteAllBranchClients(Client_tree* clientHead );
-Client_tree* deleteClient(Client_tree* clientHead , int clientId );
-Client_tree* findDeleteClient(Client_tree* clientNode , int clientId , Client_tree** parent);
-Client_tree* find_max_client(Client_tree* clientNode);
-Client_tree* find_min_client(Client_tree* clientNode);
-void freeClient(Client_tree* clientNode);
+Tree* deleteAllBranchClients(Tree* clientHead );
+Tree* deleteClient(Tree* clientHead , int clientId );
+Tree* findDeleteClient(Tree* clientNode , int clientId , Tree** parent);
+Tree* find_max_client(Tree* clientNode);
+Tree* find_min_client(Tree* clientNode);
+void freeClient(Tree* clientNode);
 
 
 /*function's manage all client changes*/
@@ -50,7 +50,7 @@ void loanToClient(Client* client , Branch* branch);
 void repayClientLoans(Client* client , Branch* branch);
 int chekLoanRequest(double authorizedException , double deposit , double loanBalance);
 void printClientDetails(Client* client);
-void printClientId(Client_tree* clientHead );
+void printClientId(Tree* clientHead );
 
 
 #endif
